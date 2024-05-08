2de1cf0a760f92b7e83b04653c4d0ef3ca0d42b8
gijsn <61279464+gijsn@users.noreply.github.com>
2024-05-08 21:19:37 +0200

    add function prototypes

--- src/main.cpp
+++ src/main.cpp
@@ -13,6 +13,83 @@
 #include "AR488_ComPorts.h"
 #include "AR488_Eeprom.h"
 
+void initDevice();
+void initController();
+uint8_t serialIn_h();
+void errBadCmd();
+uint8_t parseInput(char c);
+bool isCmd(char *buffr);
+bool isIdnQuery(char *buffr);
+bool isRead(char *buffr);
+void addPbuf(char c);
+void flushPbuf();
+void showPrompt();
+void sendToInstrument(char *buffr, uint8_t dsize);
+void execCmd(char *buffr, uint8_t dsize);
+void getCmd(char *buffr);
+bool notInRange(char *param, uint16_t lowl, uint16_t higl, uint16_t &rval);
+
+void addr_h(char *params);
+void rtmo_h(char *params);
+void eos_h(char *params);
+void eoi_h(char *params);
+void cmode_h(char *params);
+void eot_en_h(char *params);
+void eot_char_h(char *params);
+void amode_h(char *params);
+void ver_h(char *params);
+void read_h(char *params);
+void clr_h();
+void llo_h(char *params);
+void loc_h(char *params);
+void ifc_h();
+void trg_h(char *params);
+void rst_h();
+void spoll_h(char *params);
+void srq_h();
+void stat_h(char *params);
+void save_h();
+void lon_h(char *params);
+void help_h(char *params);
+void aspoll_h();
+void dcl_h();
+void default_h();
+void eor_h(char *params);
+void ppoll_h();
+void ren_h(char *params);
+void verb_h();
+void setvstr_h(char *params);
+void prom_h(char *params);
+void ton_h(char *params);
+void srqa_h(char *params);
+void repeat_h(char *params);
+void tct_h(char *params);
+void macro_h(char *params);
+void xdiag_h(char *params);
+void id_h(char *params);
+void idn_h(char * params);
+void sendmla_h();
+void sendmta_h();
+void sendmsa_h(char *params);
+void unlisten_h();
+void untalk_h();
+
+void attnRequired();
+void execGpibCmd(uint8_t gpibcmd);
+
+
+void device_listen_h();
+void device_talk_h();
+void device_sdc_h();
+void device_spd_h();
+void device_spe_h();
+void device_tct_h();
+bool device_unl_h();
+bool device_unt_h();
+
+
+void lonMode();
+void tonMode();
 
 /***** FWVER "AR488 GPIB controller, ver. 0.51.29, 18/03/2024" *****/
 
@@ -2172,7 +2249,10 @@ void id_h(char *params) {
 #endif
           memset(gpibBus.cfg.vstr, '\0', 48);
           strncpy(gpibBus.cfg.vstr, datastr, dlen);
-          if (isVerb) dataPort.print(F("VerStr: "));dataPort.println(gpibBus.cfg.vstr);
+          if (isVerb) {
+            dataPort.print(F("VerStr: "));
+            dataPort.println(gpibBus.cfg.vstr);
+          }
         }else{
           if (isVerb) dataPort.println(F("Length of version string must not exceed 48 characters!"));
           errBadCmd();
